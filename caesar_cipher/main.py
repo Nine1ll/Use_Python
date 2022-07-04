4938ba19d87c14d78e6d5d345a734f39a1eb4a24
GwangYoung <gospotb1607@gmail.com>
2022-07-04 10:11:41 +0900

    caesar_cipher_complete.ver

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -1,16 +1,31 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char not in alphabet :
+      end_text+=char
+    else : 
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    new_letter = alphabet[new_position]
-    cipher_text += new_letter
-  print(f"The encoded text is {cipher_text}")
+import art
+print(art.logo)
 
-encrypt(plain_text=text, shift_amount=shift)
+restart = "yes"
+while(restart == "yes") :
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  if shift >= len(alphabet)/2 :
+    shift=int(shift%(len(alphabet)/2))
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'\n").lower()
+print("Thanks for playing.")
