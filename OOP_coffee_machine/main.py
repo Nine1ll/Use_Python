9f8adb7b5bc1ee5466eb024562b9c4801035e0c6
GwangYoung <gospotb1607@gmail.com>
2022-07-21 07:58:24 +0900

    opp_coffe_machine

--- OOP_coffee_machine/main.py
+++ OOP_coffee_machine/main.py
@@ -2,3 +2,23 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_machine = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+while is_on:
+    order = input(f"What would you like? ({menu.get_items()}): ")
+    if order == "off":
+        is_on = False
+    elif order == "report":
+        coffee_machine.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if money_machine.make_payment(drink.cost) and coffee_machine.is_resource_sufficient(drink):
+            coffee_machine.make_coffee(drink)
+
+
+
+
