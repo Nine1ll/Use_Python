d6c04c59f678479189c4ff8d99001984e6b09d5e
GwangYoung <gospotb1607@gmail.com>
2022-08-16 13:23:02 +0900

    stock_news_complete

--- stock_news/main.py
+++ stock_news/main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -6,6 +8,9 @@ STOCK_API_KEY = "8ED7Z23MK1O4XGVP"
 NEWS_API_KEY = "791e477e438448fbb57b13e9f3d34d1b"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+TWILIO_SID = "AC7b5a93187ca63b913e6d4638addbe412"
+TWILIO_AUTH_TOKEN = '2e888799ce323e538d119efe65c9fb69'
 
 stock_parameters = {
     'function': 'TIME_SERIES_DAILY',
@@ -25,46 +30,36 @@ the_day_before_data = data_list[1]
 the_day_before_closing_price = float(the_day_before_data['4. close'])
 print(the_day_before_closing_price)
 
-positive_difference = round(abs(yesterday_closing_price - the_day_before_closing_price), 4)
-print(positive_difference)
+difference = round(abs(yesterday_closing_price - the_day_before_closing_price), 4)
 
-percentage = round((positive_difference/the_day_before_closing_price)*100,2)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+percentage = round((difference / yesterday_closing_price) * 100, 2)
 print(percentage)
-
-date = list(response.json()['Time Series (Daily)'].keys())
 
 # Get News
 
 news_parameters = {
     'q': COMPANY_NAME,
-    'from': date,
     'apiKey': NEWS_API_KEY
 }
 
-if True: # percentage > 5
+if percentage > 5:
+
     response_news = requests.get(NEWS_ENDPOINT, params=news_parameters)
-    news = response_news.json()['articles'][:3]
-    headlines = [news_dict['title'] for news_dict in news]
-    print(headlines)
+    articles = response_news.json()['articles']
+    three_articles = articles[:3]
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{percentage}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    print(formatted_articles)
 
-# # STEP 3: Use twilio.com/docs/sms/quickstart/python
-# to send a separate message with each article's title and description to your phone number.
-
-# TODO 8. - Create a new list of the first 3 article's headline
-# and description using list comprehension.
-
-
-# TODO 9. - Send each article as a separate message via Twilio.
-
-# Optional
-# TODO: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_='+19705362481',
+            to='+821037089283'
+        )
