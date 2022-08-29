b259d8000986788950b89f76d06de1615db67894
GwangYoung <gospotb1607@gmail.com>
2022-08-29 19:02:14 +0900

    flight-deal-start

--- flight-deals/flight_search.py
+++ flight-deals/flight_search.py
@@ -1,3 +1,6 @@
+API_KEY = 'XijaTxsyoFPkZey82f0VGtTH9ZIuG0-B'
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
+    # This class is responsible for talking to the Flight Search API.
     pass
--- flight-deals/main.py
+++ flight-deals/main.py
@@ -1,1 +1,13 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes
+# to achieve the program requirements.
+
+# TODO: 항공권 검색 API와 Sheety API를 사용하여, 도시별 국제항공운송협회(IATA) 코드를 구글 시트 사본에 붙이세요.
+#  시트에 있는 대부분의 도시에는 공항이 여러 개 있으므로 (여기에서 공항 코드가 아니라) 도시 코드를 참고하세요.
+
+# TODO: 항공권 검색 API를 사용하여 구글 시트에 있는 모든 도시에 대해 내일부터 6개월간 가장 저렴한 항공권을 확인하세요.
+
+# TODO: 가격이 구글 시트에 있는 최저가보다 저렴하다면, 트윌리오 API를 사용하여 본인 전화번호로 SMS를 보내세요.
+
+# TODO: SMS에는 출발공항 IATA 코드, 도착공항 IATA 코드,
+
+# TODO: 출발 도시,도착 도시,항공권 가격,비행 날짜가 있어야 합니다.
