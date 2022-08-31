182685e818d777e2b08d6ac8046a2e8f2c48f5bd
GwangYoung <gospotb1607@gmail.com>
2022-08-31 23:12:03 +0900

    pomodoro_ui 개선 작업 진행 중

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -86,17 +86,19 @@ title.grid(column=1, row=0)
 
 # Tomato Img &  Timer
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
+tomato_img = PhotoImage(file="image/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 # Start Button
-start_button = Button(text="Start", highlightthickness=0, bg="white", command=start_timer)
+start_image = PhotoImage(file="image/play.png")
+start_button = Button(text="Start", highlightthickness=0, bg="white", command=start_timer, image=start_image)
 start_button.grid(column=0, row=2)
 
 # Reset Button
-reset_button = Button(text="Reset", highlightthickness=0, bg="white", command=reset_timer)
+stop_image = PhotoImage(file="image/stop.png")
+reset_button = Button(text="Reset", highlightthickness=0, bg="white", command=reset_timer, image=stop_image)
 reset_button.grid(column=2, row=2)
 
 # Check Label
